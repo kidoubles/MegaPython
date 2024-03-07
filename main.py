dcb9f10a66f2cb376e46e312e03be5ca7cf8977b
Reg Armstrong <reg@c3times.com>
2024-03-07 11:46:31 +1000

    readme file added

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 user_prompt = "Enter a to do: "
-user_text = input(user_prompt)
-print(user_text)
+user_text1 = input(user_prompt)
+user_text2 = input(user_prompt)
+user_text3 = input(user_prompt)
 
+to_do = [user_text1, user_text2, user_text3]
+print(to_do)
+
+print(type(to_do))  # this prints out the data type of the variable to_do, can change it to user_prompt or user_text
 
